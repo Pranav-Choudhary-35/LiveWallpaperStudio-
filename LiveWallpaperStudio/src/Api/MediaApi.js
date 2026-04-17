e06cc9331eabe80f115b8a7c9d991519793411e5
Pranav-Choudhary-35 <pranavchoudhary498@gmail.com>
2026-04-17 23:07:35 +0530

    Add API's like Fetch Images,videos,GIF's

---
+++ LiveWallpaperStudio/src/Api/MediaApi.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+
+const UNSPLASH_KEY=import.meta.env.VITE_UNSPLASH_KEY;
+const PEXELS_KEY=import.meta.env.VITE_UNSPLASH_KEY;
+const GIPHY_KEY=import.meta.env.VITE_GIPHY_KEY;
+
+
+export async function fetchImages(query,page=1,per_page=20){
+
+const res=await axios.get('https://api.unsplash.com/photos',{
+
+params:{
+    query,page,per_page
+},
+headers:{
+Authorization:`Client-ID ${UNSPLASH_KEY}`
+}    
+
+
+})    
+console.log(res.data);
+
+
+}
+
+
+export async function fetchVideos(query,per_page=20){
+
+const res=await axios.get('https://api.pexels.com/videos/search',{
+
+    params:{
+query,per_page
+    },
+    headers:{
+Authorization:PEXELS_KEY
+    }
+
+
+})
+console.log(res.data.videos);
+
+
+}
+
+export async function  fetchGif(query) {
+    
+const res=await axios.get('https://api.giphy.com/v1/gifs/search',{
+ params: {
+    api_key: GIPHY_KEY,
+    q: query,
+    limit: 10 // Number of results
+  }
+})
+console.log(res.data.data);
+
+}
